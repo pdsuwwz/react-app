d5046f72ab7649f7a077eb4b82ab56a05ce60bbd
pdsuwwz <pdsu.wwz@foxmail.com>
2019-01-18 14:50:03 +0800

    :watermelon: change to configurable babelrc

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -23,62 +23,82 @@ module.exports = {
   module: {
     rules: [
       {
-      enforce: 'pre',
-      test: /\.(jsx?)$/,
-      loader: 'eslint-loader',
-      include: resolve('src'),
-      options: {
-        fix: true,
-        cache: resolve('.cache/eslint'),
-        failOnError: true, // 生产环境发现代码不合法，则中断编译
-        useEslintrc: true,
-        configFile: resolve('.eslintrc.js'),
-        formatter: require('eslint-friendly-formatter'),
-        baseConfig: {
-          extends: [resolve('.eslintrc.js')]
+        enforce: 'pre',
+        test: /\.(jsx?)$/,
+        loader: 'eslint-loader',
+        include: resolve('src'),
+        options: {
+          fix: true,
+          // cache: resolve('.cache/eslint'),
+          failOnError: true, // 生产环境发现代码不合法，则中断编译
+          useEslintrc: true,
+          configFile: resolve('.eslintrc.js'),
+          formatter: require('eslint-friendly-formatter'),
+          // baseConfig: {
+          //   extends: [resolve('.eslintrc.js')]
+          // }
         }
-      }
-    }, 
-    {
-      test: /\.js|jsx$/,
-      exclude: /node_modules/,
-      loader: "babel-loader"
-    }, {
-      test: /\.scss/,
-      use: [MiniCssExtractPlugin.loader, "css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]", {
-        loader: 'postcss-loader',
-        options: { ident: 'postcss', sourceMap: true, config: { path: resolve('postcss.config.js') } },
-      }, "sass-loader"],
-      exclude: resolve('node_modules'),
-      include: resolve('src')
-    }, {
-      test: /\.css/,
-      use: [MiniCssExtractPlugin.loader, "css-loader"],
-    },
-    {
-      test: /\.(png|jpe?g|bmp|gif|webp|svg)(\?.*)?$/,
-      loader: 'url-loader',
-      options: {
-        limit: 8192,
-        name: 'assets/img/[name].[hash:7].[ext]'
-      }
-    },
-    {
-      test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-      loader: 'url-loader',
-      options: {
-        limit: 8192,
-        name: 'assets/media/[name].[hash:7].[ext]'
-      }
-    },
-    {
-      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-      loader: "url-loader",
-      options: {
-        limit: 8192,
-        name: 'assets/fonts/[name].[hash:7].[ext]'
-      }
-    }]
+      },
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            // cacheDirectory: resolve('.cache/babel'),
+            extends: resolve('babelrc.js')
+          }
+        },
+      }, {
+        test: /\.scss/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            camelCase: true,
+            importLoaders: 1,
+            localIdentName: '[name]_[local]_[hash:base64:5]',
+          },
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            sourceMap: true,
+            config: {
+              path: resolve('postcss.config.js'),
+            },
+          },
+        }, "sass-loader"],
+        exclude: resolve('node_modules'),
+        include: resolve('src')
+      }, {
+        test: /\.css/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|bmp|gif|webp|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          name: 'assets/img/[name].[hash:7].[ext]',
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          name: 'assets/media/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 8192,
+          name: 'assets/fonts/[name].[hash:7].[ext]'
+        }
+      }]
   },
   plugins: [
     // 部分插件默认已经支持，无需再次配置，详见文档 https://webpack.js.org/migrate/4/#deprecated-removed-plugins
